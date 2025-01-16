bed8b4cae29798d43b771f4bc2ad0b2cb7e928ad
Furqan Ali <furqan.ali@emumba>
2025-01-16 16:00:10 +0500

    sm'

---
+++ Order_service/Client/restaurantClient.py
@@ -0,0 +1,26 @@
+import requests
+from Schemas import schemas
+from EnviornmentVariable import enVVar
+
+def create_token(payload:schemas.payload):
+
+    url =enVVar.CREATE_TOKEN_URL
+
+    response = requests.post(url, json=payload.dict())
+
+    # Printing response
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+def refresh_token(payload:schemas.refresh_token):
+    url = enVVar.REFRESH_TOKEN_URL
+
+    response = requests.post(url, json=payload.dict())
+
+    # Printing response
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
