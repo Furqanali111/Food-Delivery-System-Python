2576afec97da8961580d81d304ff4489e012a06d
Furqan Ali <furqanali111366@gmail.com>
2025-02-13 17:12:23 +0500

    s

--- User_service/Client/authClient.py
+++ User_service/Client/authClient.py
@@ -1,14 +1,12 @@
+<<<<<<< HEAD
+# Your code here (the one you're merging now)
 import requests
 from Schemas import schemas
 from EnviornmentVariable import enVVar
 
 def create_token(payload:schemas.auth_payload):
-
     url =enVVar.CREATE_TOKEN_URL
-
     response = requests.post(url, json=payload.dict())
-
-    # Printing response
     if response.status_code == 200:
         return response.json()
     else:
@@ -16,11 +14,31 @@ def create_token(payload:schemas.auth_payload):
 
 def refresh_token(payload:schemas.refresh_token):
     url = enVVar.REFRESH_TOKEN_URL
-
     response = requests.post(url, json=payload.dict())
-
-    # Printing response
     if response.status_code == 200:
         return response.json()
     else:
         print("Error:", response.status_code, response.text)
+=======
+# Upstream code here (the company's version)
+# It might have some changes to the same code you wrote
+import requests
+from Schemas import schemas
+from EnviornmentVariable import enVVar
+
+def create_token(payload:schemas.auth_payload):
+    url =enVVar.CREATE_TOKEN_URL
+    response = requests.post(url, json=payload.dict())
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+def refresh_token(payload:schemas.refresh_token):
+    url = enVVar.REFRESH_TOKEN_URL
+    response = requests.post(url, json=payload.dict())
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+>>>>>>> upstream/master
